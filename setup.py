cf967d310ee5b737bd85c7fc1bf1c52178cc5f45
Cs-Andor <124708604+Cs-Andor@users.noreply.github.com>
2023-02-28 21:02:15 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+setup(
+    name="evesmunka-by-csete-andor",
+    version="1.0.0",
+    description="Évesmunka program 2022-23. Készítette Csete Andor",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[
+        "feedparser", "html2text", "importlib_resources", "typing"
+    ],
+    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)
